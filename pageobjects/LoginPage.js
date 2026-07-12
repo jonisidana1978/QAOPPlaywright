088f37405ec51e603f643f4e635e9a0fc0ccd92f
Sumedha <pallavsidana@yahoo.com>
2026-07-12 09:49:41 +0530

    Update login page object

--- pageobjects/LoginPage.js
+++ pageobjects/LoginPage.js
@@ -1,22 +1,25 @@
-class LoginPage{
-
-    constructor(page){
+class LoginPage {
+    constructor(page) {
         this.page = page;
-        this.signInButton=  page.locator ("#login");
-        this.userName = page.locator('#userEmail');
+        this.signInButton = page.locator("#login");
+        this.userName = page.locator("#userEmail");
         this.password = page.locator("#userPassword");
     }
-async goTO(){
-    await this.page.goto("https://rahulshettyacademy.com/client");
-    //await this.page.goTO("/client");
-}
-   async validLogin(username,password){
 
-        
+    async goTo() {
+        await this.page.goto("https://rahulshettyacademy.com/client");
+    }
+
+    async goTO() {
+        await this.goTo();
+    }
+
+    async validLogin(username, password) {
         await this.userName.fill(username);
         await this.password.fill(password);
         await this.signInButton.click();
-        await this.page.waitForLoadState('networkidle');
+        await this.page.waitForLoadState("networkidle");
     }
 }
-module.exports ={LoginPage};
+
+module.exports = { LoginPage };
